17c751f0ced8ff016b1ab0f74ee2508675b0a9d0
Ismll <ismll.acc@mail.ru>
2024-08-02 12:10:12 +0400

    The main page is ready, added base html and used extends

--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
 from tkinter.messagebox import RETRY
 from django.shortcuts import render
+from django.template import context
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Home',
+        'content': 'Магазин мебели HOME',
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'About',
+        'content': 'О нас',
+        'text': 'О том, почему наш магазин самый классный..'
+    }
+    return render(request, 'main/about.html', context)
