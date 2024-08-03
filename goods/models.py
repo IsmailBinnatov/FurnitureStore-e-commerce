3fc77b7610db4fc18f0ea5c43f4d5615880c001f
Ismll <ismll.acc@mail.ru>
2024-08-03 11:27:23 +0400

    added Category model and register

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,15 @@
+from os import name
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    slug = models.SlugField(unique=True, verbose_name='URL',
+                            max_length=200, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
