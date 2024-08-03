8ac470007d0b126b5aa6f22f0aaf3411929526b2
Ismll <ismll.acc@mail.ru>
2024-08-03 12:32:30 +0400

    added fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from goods.models import Category
+from goods.models import Category, Product
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- goods/models.py
+++ goods/models.py
@@ -1,4 +1,6 @@
+from email.mime import image
 from os import name
+from unicodedata import category
 from django.db import models
 
 
@@ -8,8 +10,36 @@ class Category(models.Model):
                             max_length=200, null=True, blank=True)
 
     class Meta:
+        db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True,
+                            verbose_name='Название')
+    slug = models.CharField(max_length=200, unique=True,
+                            blank=True, null=True, verbose_name='URL',)
+    description = models.TextField(
+        blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(
+        upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(
+        default=0, verbose_name='Количество')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
     def __str__(self):
         return self.name
