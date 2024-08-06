bfcb9f36c7d021a678bae728baef6fe21f75e40e
Ismll <ismll.acc@mail.ru>
2024-08-06 12:21:09 +0400

    added products filter and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from django.utils.http import urlencode
 from django import template
 
 from goods.models import Category
@@ -6,6 +7,13 @@ from goods.models import Category
 register = template.Library()
 
 
-@register.simple_tag
+@register.simple_tag()
 def tag_categories():
     return Category.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
