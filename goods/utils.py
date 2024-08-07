028d1d34dcf75c9b4737294af9fd8627617ae600
Ismll <ismll.acc@mail.ru>
2024-08-07 18:48:44 +0400

    added full text search (SearchVector)

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Product
 
@@ -7,13 +8,20 @@ def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Product.objects.filter(id=int(query))
+    
+    
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    
+    return Product.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    
+    
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    keywords = [word for word in query.split() if len(word) > 2]
-    
-    q_objects = Q()
-    
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-        
-    return Product.objects.filter(q_objects)
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Product.objects.filter(q_objects)
